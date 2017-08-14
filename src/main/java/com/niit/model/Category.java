7a0823494c53f1f78a20543d436c9a130ac4eb30
piriyangha <t.piriyangha@gmail.com>
2017-08-14 18:02:28 +0800

    category

--- src/main/java/com/niit/model/Category.java
+++ src/main/java/com/niit/model/Category.java
@@ -1,5 +1,20 @@
 package com.niit.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Category")
 public class Category {
+	@Id
+	@GeneratedValue (strategy=GenerationType.AUTO)
+	 private int cid;
+	 private String catname;
+	
+	
+	
 
 }
